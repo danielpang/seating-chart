3d4dd562d961057818b0d3bca5e7d64b084e82f5
Daniel Pang <d_pang@mac.home>
2025-02-24 02:45:26 -0500

    remove CORS

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -5,12 +5,7 @@ export default async (req, context) => {
     const input = req.headers.get("password");
     
     if (input === spreadsheetPassword) {
-        const headers = {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-        }
-        return new Response("success", { headers: headers});
+        return new Response("success");
     }
     return new Response("Unauthorized", { status: 401 });
 };
